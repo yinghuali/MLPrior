bbacefab3697cb03f412839e239405c67ece224f
yinghuali <yinghua.li@uni.lu>
2023-05-14 13:57:02 +0200

    update

--- mutation_config.py
+++ mutation_config.py
@@ -1,88 +1,65 @@
 
 dic_mutation_heart_rf = {
-                   'n_estimators': [10, 20, 30, 40],
-                   'max_depth': [5, 10, 20],
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
 
 
 dic_mutation_heart_xgboost = {
-                   'n_estimators': [1, 2, 3, 5, 10],
-                   'max_depth': [2, 3],
-                   'eta': [0.1, 3],
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
 
 dic_mutation_heart_lgb = {
-                   'n_estimators': [1, 2, 3, 5, 10],
-                   'max_depth': [2, 3],
-                   'num_leaves': [10, 30, 50],
-                   'min_data_in_leaf': [40],
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
 
 
 dic_mutation_adult_rf = {
-                   'n_estimators': [5, 10, 15, 20, 25, 30],
-                   'max_depth': [2, 3],
-                   'max_features': [1, 2, 3],
-                   'min_samples_split': [2, 3],
-                   'min_samples_leaf': [1, 2, 3]
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
 
 
 dic_mutation_adult_xgboost = {
-                   'n_estimators': [5, 10, 15, 20, 25, 30],
-                   'max_depth': [2, 3],
-                   'eta': [0.1, 3],
-                   'gamma': [1, 10, 20],
-                   'min_child_weight': [1, 10],
-                   'refresh_leaf': [0, 1]
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
 
 dic_mutation_adult_lgb = {
-                   'n_estimators': [5, 10, 15, 20, 25, 30],
-                   'max_depth': [2, 3],
-                   'num_leaves': [10, 30, 50],
-                   'min_data_in_leaf': [40],
-                   'bagging_fraction': [0.1, 0.9],
-                   'pos_bagging_fraction': [0.1, 0.9],
-                   'lambda_l1': [1, 10]
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
 
 
 dic_mutation_bank_rf = {
-                   'n_estimators': [10, 20, 30, 40],
-                   'max_depth': [5, 10, 20],
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
-
 
 dic_mutation_bank_xgboost = {
-                   'n_estimators': [1, 2, 3, 5, 10],
-                   'max_depth': [2, 3],
-                   'eta': [0.1, 3],
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
-
 dic_mutation_bank_lgb = {
-                   'n_estimators': [1, 2, 3, 5, 10],
-                   'max_depth': [2, 3],
-                   'num_leaves': [10, 30, 50],
-                   'min_data_in_leaf': [40],
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
 
 
 dic_mutation_stroke_rf = {
-                   'n_estimators': [10, 20, 30, 40],
-                   'max_depth': [5, 10, 20],
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
 
 
 dic_mutation_stroke_xgboost = {
-                   'n_estimators': [1, 2, 3, 5, 10],
-                   'max_depth': [2, 3],
-                   'eta': [0.1, 3],
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
 
 dic_mutation_stroke_lgb = {
-                   'n_estimators': [1, 2, 3, 5, 10],
-                   'max_depth': [2, 3],
-                   'num_leaves': [10, 30, 50],
-                   'min_data_in_leaf': [40],
+                   'n_estimators': [100, 200],
+                   'max_depth': [5, 10],
                    }
