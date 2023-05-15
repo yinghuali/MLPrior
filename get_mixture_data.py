4af5efebf81a0cb77f5eb588b707c54d97145d66
yinghuali <yinghua.li@uni.lu>
2023-05-15 20:35:00 +0200

    update

---
+++ get_mixture_data.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import random
+
+
+def get_mixture_missing_data(path, n_data, ratio, label_col_name, save_path):
+    df = pd.read_csv(path)
+    cols_list = list(df.columns)
+    cols_list.remove(label_col_name)
+    n = int(len(df)*ratio)
+    for s in range(n_data):
+        df_tmp = df.copy()
+        for i in range(n):
+            cols_name = random.sample(cols_list, 1)[0]
+            idx = random.randint(0, len(df))
+            df_tmp[cols_name][idx]=0
+        df_tmp.to_csv(save_path+'_'+str(s)+'.csv', index=False,sep=',')
+
+
+def get_mixture_noise_data(path, n_data, ratio, label_col_name, save_path):
+    df = pd.read_csv(path)
+    cols_list = list(df.columns)
+    cols_list.remove(label_col_name)
+    select_cols_list = []
+    for col in cols_list:
+        if df[col].dtype == 'int64':
+            select_cols_list.append(col)
+    n = int(len(df) * ratio)
+    for s in range(n_data):
+        df_tmp = df.copy()
+        for i in range(n):
+            cols_name = random.sample(select_cols_list, 1)[0]
+            idx = random.randint(0, len(df))
+            try:
+                df_tmp[cols_name][idx] = int(df_tmp[cols_name][idx]*random.uniform(0.5, 1)) + df_tmp[cols_name][idx]
+            except:
+                continue
+        df_tmp.to_csv(save_path+'_'+str(s)+'.csv', index=False,sep=',')
+
+
+if __name__ == '__main__':
+    get_mixture_missing_data('./data/adult.csv', 10, 0.3, 'income', './data/missing/adult_mixture_missing')
+    get_mixture_missing_data('./data/bank.csv', 10, 0.3, 'y', './data/missing/bank_mixture_missing')
+    get_mixture_missing_data('./data/stroke.csv', 10, 0.3, 'stroke', './data/missing/stroke_mixture_missing')
+    get_mixture_noise_data('./data/adult.csv', 10, 0.3, 'income', './data/noise/adult_mixture_noise')
+    get_mixture_noise_data('./data/bank.csv', 10, 0.3, 'y', './data/noise/bank_mixture_noise')
+    get_mixture_noise_data('./data/stroke.csv', 10, 0.3, 'stroke', './data/noise/stroke_mixture_noise')
+
--- test.py
+++ test.py
@@ -4,16 +4,10 @@ from utils import *
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-path_data = '/Users/yinghua.li/Documents/Pycharm/MLPrior/data/adult.csv'
-label_name = 'income'
+df = pd.read_csv('./data/adult.csv')
+print(df.head(3))
+print(df['hours-per-week'].dtypes)
 
-model = joblib.load('/Users/yinghua.li/Documents/Pycharm/MLPrior/models/target_models/adult_svm.model')
-print(model)
-
-x, y = read_data(path_data, label_name)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
-
-
-
-print(model.support_)
-
+df['hours-per-week'][0]=0
+print(df.head(3))
+print(df['hours-per-week'].dtypes)
